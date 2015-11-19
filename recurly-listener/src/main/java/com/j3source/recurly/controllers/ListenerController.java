a233abd771ec3da821345227ac3c65c67c3a0db4
jahskee3 <jahskee3@yahoo.com>
2015-11-19 17:18:03 -0500

    Signed-off-by: jahskee3 <jahskee3@yahoo.com>

--- recurly-listener/src/main/java/com/j3source/recurly/controllers/ListenerController.java
+++ recurly-listener/src/main/java/com/j3source/recurly/controllers/ListenerController.java
@@ -9,12 +9,10 @@ import javax.servlet.http.HttpServletRequest;
 import org.apache.commons.io.IOUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
-@RequestMapping("/recurly")
 public class ListenerController {
 
     @RequestMapping(value = "/listener", method = RequestMethod.POST)
