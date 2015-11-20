fa5cfe5710a3277feb8a65c4ec4eb401f586e03e
jahskee3 <jahskee3@yahoo.com>
2015-11-20 02:03:54 -0500

    Signed-off-by: jahskee3 <jahskee3@yahoo.com>

--- recurly-listener/src/main/java/com/j3source/recurly/controllers/listener/unmarshall/UnmarshallUtil.java
+++ recurly-listener/src/main/java/com/j3source/recurly/controllers/listener/unmarshall/UnmarshallUtil.java
@@ -20,6 +20,7 @@ import com.j3source.recurly.controllers.listener.events.notifications.NewSubscri
 import com.j3source.recurly.controllers.listener.events.notifications.PastDueInvoiceNotification;
 import com.j3source.recurly.controllers.listener.events.notifications.ProcessingInvoiceNotification;
 import com.j3source.recurly.controllers.listener.events.notifications.ReactivatedAccountNotification;
+import com.j3source.recurly.controllers.listener.events.notifications.UpdatedSubscriptionNotification;
 
 public class UnmarshallUtil {
 
@@ -50,7 +51,9 @@ public class UnmarshallUtil {
 		
 		String newSubscriptionNotificationData="<?xml version='1.0' encoding='UTF-8'?> <new_subscription_notification> <account> <account_code>1</account_code> <username nil='true'>verena</username> <email>verena@example.com</email> <first_name>Verena</first_name> <last_name>Example</last_name> <company_name nil='true'>Company, Inc.</company_name> </account> <subscription> <plan> <plan_code>bronze</plan_code> <name>Bronze Plan</name> <version type='integer'>2</version> </plan> <uuid>8047cb4fd5f874b14d713d785436ebd3</uuid> <state>active</state> <quantity type='integer'>2</quantity> <total_amount_in_cents type='integer'>2000</total_amount_in_cents> <activated_at type='datetime'>2009-11-22T13:10:38Z</activated_at> <canceled_at type='datetime'></canceled_at> <expires_at type='datetime'></expires_at> <current_period_started_at type='datetime'>2009-11-22T13:10:38Z</current_period_started_at> <current_period_ends_at type='datetime'>2009-11-29T13:10:38Z</current_period_ends_at> <trial_started_at type='datetime'>2009-11-22T13:10:38Z</trial_started_at> <trial_ends_at type='datetime'>2009-11-29T13:10:38Z</trial_ends_at> <collection_method>automatic</collection_method> </subscription> </new_subscription_notification>";
 		UnmarshallUtil.eventAction(newSubscriptionNotificationData);
-				
+
+		String updatedSubscriptionNotificationData="<?xml version='1.0' encoding='UTF-8'?> <updated_subscription_notification> <account> <account_code>1</account_code> <username nil='true'></username> <email>verena@example.com</email> <first_name>Verena</first_name> <last_name>Example</last_name> <company_name nil='true'></company_name> </account> <subscription> <plan> <plan_code>1dpt</plan_code> <name>Subscription One</name> </plan> <uuid>292332928954ca62fa48048be5ac98ec</uuid> <state>active</state> <quantity type='integer'>1</quantity> <total_amount_in_cents type='integer'>200</total_amount_in_cents> <activated_at type='datetime'>2010-09-23T22:12:39Z</activated_at> <canceled_at nil='true' type='datetime'></canceled_at> <expires_at nil='true' type='datetime'></expires_at> <current_period_started_at type='datetime'>2010-09-23T22:03:30Z</current_period_started_at> <current_period_ends_at type='datetime'>2010-09-24T22:03:30Z</current_period_ends_at> <trial_started_at nil='true' type='datetime'> </trial_started_at> <trial_ends_at nil='true' type='datetime'> </trial_ends_at> <collection_method>automatic</collection_method> </subscription> </updated_subscription_notification>";
+		UnmarshallUtil.eventAction(updatedSubscriptionNotificationData);
 	}
 	
 	public static void eventAction(String xmlData) throws JDOMException, IOException, JAXBException{
@@ -73,6 +76,8 @@ public class UnmarshallUtil {
 			System.out.println("Past Due Invoice : "+event.toString());
 		}else if(event instanceof NewSubscriptionNotification){
 			System.out.println("New Subscription : "+event.toString());
+		}else if(event instanceof UpdatedSubscriptionNotification){
+			System.out.println("Updated Subscription : "+event.toString());
 		}
 	}
 	
@@ -97,6 +102,8 @@ public class UnmarshallUtil {
 			notificationClass=PastDueInvoiceNotification.class;
 		}else if(rootElementName.equals("new_subscription_notification")){
 			notificationClass=NewSubscriptionNotification.class;
+		}else if(rootElementName.equals("updated_subscription_notification")){
+			notificationClass=UpdatedSubscriptionNotification.class;
 		}
 		
 		return notificationClass;
