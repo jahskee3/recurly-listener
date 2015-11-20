f0dae8b8c32f17544520178ee207b9d7d51105c8
jahskee3 <jahskee3@yahoo.com>
2015-11-19 20:25:19 -0500

    Signed-off-by: jahskee3 <jahskee3@yahoo.com>

--- recurly-listener/src/main/java/com/j3source/recurly/controllers/ListenerController.java
+++ recurly-listener/src/main/java/com/j3source/recurly/controllers/ListenerController.java
@@ -5,27 +5,33 @@ import java.io.InputStream;
 import java.io.StringWriter;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.xml.bind.JAXBException;
 
 import org.apache.commons.io.IOUtils;
+import org.jdom2.JDOMException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.j3source.recurly.unmarshall.UnmarshallTest;
+
 @Controller
 public class ListenerController {
 
     @RequestMapping(value = "/listener", method = RequestMethod.POST)
-    public String listenerPost(ModelMap model, HttpServletRequest request) throws IOException {
+    public void listenerPost(ModelMap model, HttpServletRequest request) throws IOException, JDOMException, JAXBException {
     	String host = request.getHeader("Host");
     	String contentType = request.getHeader("Content-Type");
     	InputStream xml = request.getInputStream();
     	
     	StringWriter writer = new StringWriter();
     	IOUtils.copy(xml, writer, "UTF-8");
-    	String theString = writer.toString();
-        model.addAttribute("msg", "Host:"+host);
-        return "hello";
+    	String xmlData = writer.toString();
+        
+    	UnmarshallTest.eventAction(xmlData);
+    	
+        
     }
     
 	@RequestMapping(value = "/listener", method = RequestMethod.GET)
--- recurly-listener/src/main/java/com/j3source/recurly/unmarshall/UnmarshallTest.java
+++ recurly-listener/src/main/java/com/j3source/recurly/unmarshall/UnmarshallTest.java
@@ -11,6 +11,7 @@ import org.jdom2.Document;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 
+import com.j3source.recurly.events.notifications.BillingInfoUpdatedNotification;
 import com.j3source.recurly.events.notifications.CanceledAccountNotification;
 import com.j3source.recurly.events.notifications.NewAccountNotification;
 
@@ -21,36 +22,49 @@ public class UnmarshallTest {
 	public static void main(String[] args) throws JAXBException, JDOMException, IOException {
 		String xmlData= "<?xml version='1.0' encoding='UTF-8'?><new_account_notification>  <account>    <account_code>1</account_code>    <username nil='true'>jahskee</username>    <email>verena@example.com</email>    <first_name>Verena</first_name>    <last_name>Example</last_name>    <company_name nil='true'></company_name>  </account></new_account_notification>";
 				
+		UnmarshallTest.eventAction(xmlData);
+	
+		//System.out.println("hello");
+		//System.out.println(event.toString());
+		
+	}
+	
+	public static void eventAction(String xmlData) throws JDOMException, IOException, JAXBException{
+		Object event = getEvent(xmlData);		
+		if(event instanceof CanceledAccountNotification){
+			System.out.println("Cancel account: "+event.toString());
+		}else if(event instanceof NewAccountNotification){
+			System.out.println("New account: "+event.toString());
+		}else if(event instanceof BillingInfoUpdatedNotification){
+			System.out.println("Billing info updated: "+event.toString());
+		}
+	}
+	
+	private static Object getEvent(String xmlData) throws JDOMException, IOException, JAXBException{
 		Class notificationClass = getNotificationClass(xmlData);
 		
 		JAXBContext jaxbContext = JAXBContext.newInstance(notificationClass);
 		Unmarshaller unmarshaller = jaxbContext.createUnmarshaller();
 		StringReader reader = new StringReader(xmlData);		
 		Object event = (Object) unmarshaller.unmarshal(reader);
-		
-		
-		if(event instanceof CanceledAccountNotification){
-			System.out.println("Cancel account: "+event.toString());
-		}else if(event instanceof NewAccountNotification){
-			System.out.println("New account: "+event.toString());
-		}
-		//System.out.println("hello");
-		//System.out.println(event.toString());
-		
+		return event;
 	}
 	
-	public static Class getNotificationClass(String xmlData) throws JDOMException, IOException{
+	
+	private static Class getNotificationClass(String xmlData) throws JDOMException, IOException{
 		Class notificationClass = null;
 		String rootElementName=getRootElementName(xmlData);
 		if(rootElementName.equals("canceled_account_notification")){
 			notificationClass=CanceledAccountNotification.class;
 		}else if(rootElementName.equals("new_account_notification")){
 			notificationClass=NewAccountNotification.class;
+		}else if(rootElementName.equals("billing_info_updated_notification")){
+			notificationClass=BillingInfoUpdatedNotification.class;
 		}
 		return notificationClass;
 	}
 	
-	public static String getRootElementName(String xmlData) throws JDOMException, IOException{
+	private static String getRootElementName(String xmlData) throws JDOMException, IOException{
 		SAXBuilder saxBuilder = new SAXBuilder();		
 		Document doc = saxBuilder.build(new StringReader(xmlData));
 		String notificationName = doc.getRootElement().getName();
