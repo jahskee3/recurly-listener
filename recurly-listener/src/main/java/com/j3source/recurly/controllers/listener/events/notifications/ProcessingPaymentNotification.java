b8b2bd23a6eac3b5ee36f8fa963e206547c2371b
jahskee3 <jahskee3@yahoo.com>
2015-11-20 03:40:51 -0500

    Signed-off-by: jahskee3 <jahskee3@yahoo.com>

---
+++ recurly-listener/src/main/java/com/j3source/recurly/controllers/listener/events/notifications/ProcessingPaymentNotification.java
@@ -0,0 +1,34 @@
+package com.j3source.recurly.controllers.listener.events.notifications;
+
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.j3source.recurly.controllers.listener.events.models.AccountE;
+import com.j3source.recurly.controllers.listener.events.models.TransactionE;
+
+@XmlRootElement(name = "processing_payment_notification")
+public class ProcessingPaymentNotification {
+	
+	@XmlElement(name = "account")
+	private AccountE account;
+	
+	@XmlElement(name = "transaction")
+	private TransactionE transaction;
+
+	public AccountE getAccount() {
+		return account;
+	}
+
+	public TransactionE getTransaction() {
+		return transaction;
+	}
+	
+	@Override
+	public String toString() {
+		final StringBuilder sb = new StringBuilder("ProcessingPaymentNotification{");
+		sb.append("account='").append(this.getAccount().toString()).append('\'');
+		sb.append(", transaction='").append(this.getTransaction().toString()).append('\'');
+		sb.append('}');
+		return sb.toString();
+	}
+}
