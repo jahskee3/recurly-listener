3bf95fff51f58a6ea7343c84653b74c75acb38b2
releasemgr <releasemgr@loc.veteransadvantage.com>
2015-11-21 10:08:31 -0500

    Signed-off-by: releasemgr <releasemgr@loc.veteransadvantage.com>

--- src/main/java/com/va/vauser2/controller/recurlylistener/handler/RecurlyWebhook.java
+++ src/main/java/com/va/vauser2/controller/recurlylistener/handler/RecurlyWebhook.java
@@ -1,87 +1,90 @@
 package com.va.vauser2.controller.recurlylistener.handler;
 
+import org.apache.log4j.Logger;
 import com.va.vauser2.controller.recurlylistener.events.notifications.*;
 
 public class RecurlyWebhook implements Webhook {
 
+	private static final Logger log = Logger.getLogger(RecurlyWebhook.class);
+	
 	public void billingInfoUpdateNotification(BillingInfoUpdatedNotification event) {
-		System.out.println("Billing Info Updated: "+event.toString());
+		log.info("Billing Info Updated: "+event.toString());
 	}
 
 	public void canceledAccountNotification(CanceledAccountNotification event) {
-		System.out.println("Cancel account: "+event.toString());
+		log.info("Cancel account: "+event.toString());
 	}
 
 	public void canceledSubscriptionNotification(CanceledSubscriptionNotification event) {
-		System.out.println("Cancel subscription: "+event.toString());
+		log.info("Cancel subscription: "+event.toString());
 		
 	}
 
 	public void closedInvoiceNotification(ClosedInvoiceNotification event) {
-		System.out.println("Closed invoice notification: "+event.toString());
+		log.info("Closed invoice notification: "+event.toString());
 		
 	}
 
 	public void expiredSubscriptionNotification(ExpiredSubscriptionNotification event) {
-		System.out.println("Expired subscription notification: "+event.toString());
+		log.info("Expired subscription notification: "+event.toString());
 		
 	}
 
 	public void failedPaymentNotification(FailedPaymentNotification event) {
-		System.out.println("Failed payment: "+event.toString());
+		log.info("Failed payment: "+event.toString());
 		
 	}
 
 	public void newAccountNotification(NewAccountNotification event) {
-		System.out.println("New account: "+event.toString());		
+		log.info("New account: "+event.toString());		
 	}
 
 	public void newInvoiceNotification(NewInvoiceNotification event) {
-		System.out.println("New invoice: "+event.toString());			
+		log.info("New invoice: "+event.toString());			
 	}
 
 	public void newSubscriptionNotification(NewSubscriptionNotification event) {
-		System.out.println("New subscription: "+event.toString());	
+		log.info("New subscription: "+event.toString());	
 	}
 
 	public void pastDueInvoiceNotification(PastDueInvoiceNotification event) {
-		System.out.println("Past due invoice: "+event.toString());		
+		log.info("Past due invoice: "+event.toString());		
 	}
 
 	public void processingInvoiceNotification(ProcessingInvoiceNotification event) {
-		System.out.println("Processing invoice: "+event.toString());
+		log.info("Processing invoice: "+event.toString());
 	}
 
 	public void processingPaymentNotification(ProcessingPaymentNotification event) {
-		System.out.println("Processing payment: "+event.toString());
+		log.info("Processing payment: "+event.toString());
 	}
 
 	public void reactivatedAccountNotification(ReactivatedAccountNotification event) {
-		System.out.println("Reactivated account: "+event.toString());
+		log.info("Reactivated account: "+event.toString());
 	}
 
 	public void renewedSubscriptionNotification(RenewedSubscriptionNotification event) {
-		System.out.println("Renewed subscription: "+event.toString());
+		log.info("Renewed subscription: "+event.toString());
 	}
 
 	public void scheduledPaymentNotification(ScheduledPaymentNotification event) {
-		System.out.println("Scheduled payment: "+event.toString());		
+		log.info("Scheduled payment: "+event.toString());		
 	}
 
 	public void successfulPaymentNotification(SuccessfulPaymentNotification event) {
-		System.out.println("Successful payment: "+event.toString());
+		log.info("Successful payment: "+event.toString());
 	}
 
 	public void successfulRefundNotification(SuccessfulRefundNotification event) {
-		System.out.println("Successful refund: "+event.toString());
+		log.info("Successful refund: "+event.toString());
 	}
 
 	public void updatedSubscriptionNotification(UpdatedSubscriptionNotification event) {
-		System.out.println("Updated subscription: "+event.toString());
+		log.info("Updated subscription: "+event.toString());
 	}
 
 	public void voidPaymentNotification(VoidPaymentNotification event) {
-		System.out.println("Void payment notification: "+event.toString());
+		log.info("Void payment notification: "+event.toString());
 	}	
 
 }
